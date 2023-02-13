c7c9335cfa6893285e8f6669566c4138725f3324
Stephen Krol <krol.stephen@gmail.com>
2023-02-13 21:15:24 +0100

    Add test

--- ConstantStream/ConstantStrideStream.cs
+++ ConstantStream/ConstantStrideStream.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace ConstantStream
 {
     ///<Summary>Constant byte stride sequence stream that mimic a NetworkStream.</Summary>
     public class ConstantStrideStream : Stream
     {
-        private int _position;
-        private int _size;
+        private long _position;
+        private long _size;
         private byte[] _stride;
-        private int _strideCursor;
+        private uint _strideCursor;
 
-        public ConstantStrideStream(int size, byte[] stride)
+        public ConstantStrideStream(long size, byte[] stride)
         {
             _position = 0;
             _size = size;
@@ -30,18 +31,18 @@ namespace ConstantStream
 
         public override long Position { get => _position; set => throw new NotImplementedException(); }
 
-        public static ConstantStrideStream FromNumbers(int size)
+        public static ConstantStrideStream FromNumbers(long size)
         {
             var numbers = new char[] {'0','1','2','3','4','5','6','7','8','9'};
 
             return new ConstantStrideStream(size, numbers.Cast<byte>().ToArray());
         }
 
-        public static ConstantStrideStream FromAlphabet(int size)
+        public static ConstantStrideStream FromAlphabet(long size)
         {
-            var numbers = "abcdefghijklmnopqrstuvwxyz";
+            var letters = "abcdefghijklmnopqrstuvwxyz";
 
-            return new ConstantStrideStream(size, numbers.Cast<byte>().ToArray());
+            return new ConstantStrideStream(size, Encoding.UTF8.GetBytes(letters));
         }
 
         public override void Flush()
@@ -51,7 +52,7 @@ namespace ConstantStream
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            int remaining = _size - _position;
+            long remaining = _size - _position;
             if (count == 0)
                 return 0;
 
@@ -61,21 +62,21 @@ namespace ConstantStream
                 {
                     buffer[i] = _stride[_strideCursor];
                     _strideCursor += 1;
-                    _strideCursor %= _stride.Length;
+                    _strideCursor %= (uint)_stride.Length;
                 }
 
                 _position += remaining;
-                return remaining;
+                return (int)remaining;
             }
 
             for (int i = 0; i < count; i++)
             {
                 buffer[i] = _stride[_strideCursor];
                 _strideCursor += 1;
-                _strideCursor %= _stride.Length;
+                _strideCursor %= (uint)_stride.Length;
             }
 
-            _position += count;
+            _position += (uint)count;
 
             return count;
         }
